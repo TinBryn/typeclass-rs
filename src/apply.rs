04ccff100acb13945ac5d7299f1182a2f037aa18
Kieran Griffiths <griffk04@gmail.com>
2023-01-15 16:11:34 +0800

    Added ergonomic traits for `Apply` and fixed super-traits

--- src/apply.rs
+++ src/apply.rs
@@ -1,2 +1,52 @@
 mod implementation;
 pub use implementation::{ApplyImpl, ApplyMutImpl, ApplyOnceImpl};
+
+use crate::prelude::*;
+
+pub trait Apply<A>: Higher<A>
+where
+    Self::Impl: ApplyImpl,
+{
+    fn apply<B, F: Fn(A) -> B>(self, ff: Self::With<F>) -> Self::With<B>;
+}
+
+pub trait ApplyMut<A>: Apply<A>
+where
+    Self::Impl: ApplyMutImpl,
+{
+    fn apply_mut<B, F: FnMut(A) -> B>(self, ff: Self::With<F>) -> Self::With<B>;
+}
+
+pub trait ApplyOnce<A>: ApplyMut<A>
+where
+    Self::Impl: ApplyOnceImpl,
+{
+    fn apply_once<B, F: FnOnce(A) -> B>(self, ff: Self::With<F>) -> Self::With<B>;
+}
+
+impl<A, T: HigherKind<A>> Apply<A> for T
+where
+    Self::Impl: ApplyImpl,
+{
+    fn apply<B, F: Fn(A) -> B>(self, ff: Self::With<F>) -> Self::With<B> {
+        Self::Impl::apply::<A, B, F>(self, ff)
+    }
+}
+
+impl<A, T: HigherKind<A>> ApplyMut<A> for T
+where
+    Self::Impl: ApplyMutImpl,
+{
+    fn apply_mut<B, F: FnMut(A) -> B>(self, ff: Self::With<F>) -> Self::With<B> {
+        Self::Impl::apply_mut::<A, B, F>(self, ff)
+    }
+}
+
+impl<A, T: HigherKind<A>> ApplyOnce<A> for T
+where
+    Self::Impl: ApplyOnceImpl,
+{
+    fn apply_once<B, F: FnOnce(A) -> B>(self, ff: Self::With<F>) -> Self::With<B> {
+        Self::Impl::apply_once::<A, B, F>(self, ff)
+    }
+}
--- src/apply/implementation.rs
+++ src/apply/implementation.rs
@@ -1,15 +1,15 @@
 use crate::higher::HigherImpl;
 
-pub trait ApplyOnceImpl: ApplyMutImpl {
-    fn apply_once<A, B, F: FnOnce(A) -> B>(fa: Self::Kind<A>, ff: Self::Kind<F>) -> Self::Kind<B>;
+pub trait ApplyImpl: HigherImpl {
+    fn apply<A, B, F: Fn(A) -> B>(fa: Self::Kind<A>, ff: Self::Kind<F>) -> Self::Kind<B>;
 }
 
 pub trait ApplyMutImpl: ApplyImpl {
     fn apply_mut<A, B, F: FnMut(A) -> B>(fa: Self::Kind<A>, ff: Self::Kind<F>) -> Self::Kind<B>;
 }
 
-pub trait ApplyImpl: HigherImpl {
-    fn apply<A, B, F: Fn(A) -> B>(fa: Self::Kind<A>, ff: Self::Kind<F>) -> Self::Kind<B>;
+pub trait ApplyOnceImpl: ApplyMutImpl {
+    fn apply_once<A, B, F: FnOnce(A) -> B>(fa: Self::Kind<A>, ff: Self::Kind<F>) -> Self::Kind<B>;
 }
 
 impl<T: ApplyOnceImpl> ApplyMutImpl for T {
